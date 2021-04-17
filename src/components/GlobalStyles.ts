ead7079552ba59a355001e4e4b6e079b41037a75
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-17 20:35:33 +0300

    global styles

---
+++ src/components/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 120px;
+`;
