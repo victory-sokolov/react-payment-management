3cad6eec05e99e5b2aadfbb64e148b5b182c1c33
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-19 21:14:10 +0300

    utils functions

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'development' | 'production',
+        REACT_APP_APPLE_PAY_ENDPOINT: string,
+    }
+}
---
+++ src/types.ts
@@ -0,0 +1,19 @@
+
+export interface CreditCard {
+    paymentMethod?: string
+    name: string,
+    cardNumber: string,
+    cvc: string,
+    expiryDate: Date;
+}
+
+export enum FormActionType {
+    AddNewCard,
+    MakeAPayment
+}
+
+export enum PaymentOptions {
+    CreditCard,
+    ApplePay,
+    SavedCards
+}
