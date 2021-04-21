db5f587ecdcb8776aa241ed56555b002a1a7e9b7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-21 21:19:47 +0300

    routing

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
-import { CheckoutForm } from './components/Checkout/Checkout';
-import SavedCardsContainer from './components/SavedCards/SavedCardsContainer.tsx';
+import CardsManager from './components/Cards/CardManager.tsx';
+import Timeline from './components/Timeline/Timeline.tsx';
+import NavBar from './components/Nav/Nav';
 
 export default function Routes() {
 
     return (
         <BrowserRouter>
+            <NavBar />
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/checkout" component={App} />
-                {/* <Route path="/timeline" component={} /> */}
-                <Route path="/cards" component={SavedCardsContainer} />
+                <Route path="/timeline" component={Timeline} />
+                <Route path="/cards" component={CardsManager} />
             </Switch>
         </BrowserRouter>
     )
