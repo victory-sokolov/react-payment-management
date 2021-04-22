3fd1fb0e7f9bfeb14279d14357ebc62805b10003
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-22 11:41:10 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
+import { CheckoutForm } from './components/Checkout/Checkout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <CheckoutForm />
     </div>
   );
 }
